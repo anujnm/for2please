c35dbe0f2db80134b7e4157308d85f4859d14011
Anuj Mehta <anuj.nm@gmail.com>
2013-12-29 17:16:01 +0530

    Moved js to top of page.

--- dev/wp-content/themes/fortwoplease/subscribe.php
+++ dev/wp-content/themes/fortwoplease/subscribe.php
@@ -9,7 +9,6 @@ Template Name: subscribe
 
 <head>
 	<link rel="icon" type="image/png" href="/dev/wp-content/themes/images/favicon2c.png">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu" />
 	<script type="text/javascript">
 		// Add a script element as a child of the body
 		function downloadJSAtOnload() {
@@ -20,6 +19,7 @@ Template Name: subscribe
 			var element2 = document.createElement("script");
 			element2.src = "/dev/js/jquery_cookie.js";
 			document.body.appendChild(element2);
+
 		}
 
 		// Check for browser support of event handling capability
@@ -28,6 +28,20 @@ Template Name: subscribe
 		else if (window.attachEvent)
 			window.attachEvent("onload", downloadJSAtOnload);
 		else window.onload = downloadJSAtOnload;
+
+		/* Don't need Google Analytics on Dev */
+		jQuery(document).ready(function() {
+			jQuery("#login_box").load("/dev/join", function() {
+				jQuery(".skip").show();
+			});
+		});
+		
+		jQuery("#skip_link").click(function() {
+			var date = new Date();
+			date.setTime(date.getTime() + (60 * 60 * 1000));
+			$.cookie("squeeze_popup", "show", { expires: date, path: '/' });
+			location.assign('<?php echo home_url(); ?>');
+		});
 	</script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 	<script src="/dev/js/jquery-ui-1.8.18.custom.min.js" type="text/javascript"></script>
@@ -37,6 +51,12 @@ Template Name: subscribe
 	div.head div.logo { margin: 80px 0 0 40px; background: url('/dev/wp-content/themes/images/squeeze_tagline_2.png') no-repeat; background-size: 100%; width: 70%; height: 100px; }
 	div.head img { width: 100%; height: 76px; }
 	div.skip { background: url('/dev/wp-content/themes/images/sign_up_skip_box.png') no-repeat; margin-top: 10px; width: 518px; height: 94px; padding: 26px 0; color: white; font-size: 16px; }
+	@font-face {
+		font-family: 'Ubuntu';
+		font-style: normal;
+		font-weight: 400;
+		src: local('Ubuntu'), url(https://themes.googleusercontent.com/static/fonts/ubuntu/v4/vRvZYZlUaogOuHbBTT1SNevvDin1pK8aKteLpeZ5c0A.woff) format('woff');
+	}
 	</style>
 	
 	<title>ForTwoPlease - Join us today!</title>
@@ -56,35 +76,6 @@ Template Name: subscribe
 		<p style=" margin-top: 4px;"><a href="#" id="skip_link" style="font-size: 20px; font-weight: bold;">Just let me in!</a></p>
 	</div>
 </div>
-
-
-<script type="text/javascript">
-/* Don't need Google Analytics on Dev
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-22573395-1']);
-	_gaq.push(['_setDomainName', 'fortwoplease.com']);
-	_gaq.push(['_trackPageview']);
-	
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-	*/
-	jQuery(document).ready(function() {
-		jQuery("#login_box").load("/dev/join", function() {
-			jQuery(".skip").show();
-		});
-	});
-	
-	jQuery("#skip_link").click(function() {
-		var date = new Date();
-		date.setTime(date.getTime() + (60 * 60 * 1000));
-		$.cookie("squeeze_popup", "show", { expires: date, path: '/' });
-		location.assign('<?php echo home_url(); ?>');
-	});
-
-</script>
 
 <?php
 
