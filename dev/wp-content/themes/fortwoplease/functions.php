d6d76be5fd69e90edd2104a18658935623d6374b
Anuj Mehta <anuj.nm@gmail.com>
2014-01-08 14:00:29 +0530

    Integrated Mandrill for order confirmation email.

--- dev/wp-content/themes/fortwoplease/functions.php
+++ dev/wp-content/themes/fortwoplease/functions.php
@@ -367,7 +367,6 @@ function pp_action() {
 		error_log ($httpParsedResponseAr['TIMESTAMP']);
 		error_log ('DoDirectPayment failed: ' . print_r($httpParsedResponseAr, true));
 	}*/
-	
 	require_once('stripe-php-1.10.1/lib/Stripe.php');
 	$trialAPIKey = "sk_test_2Dx34r6YqUHebwPHUEoqf1JC"; // These are the SECRET keys!
 	$token = $_POST['stripeToken'];
@@ -424,7 +423,6 @@ function pp_action() {
                                 )
 
             ));
-
             if ($charge->paid != true) {
             	throw new Exception("Error while processing charge. Please try again later. ");
             }
@@ -447,9 +445,19 @@ function pp_action() {
             } else {
             	$numberVouchers = $numberp." vouchers";
             }
-            wp_mail($email, 'Purchase Successful!', '<p style="margin:0;"><strong>Congratulations,</strong></p><p style="margin:0;">Your purchase of '.$numberVouchers.' of '.$pname.' from '.$bname.' was successful!</p><br/><p style="margin:0;"><b>Payment Summary</b></p><p style="margin:0;">Total: $'.$price.'</p><p style="margin:0;">Confirmation Number: '.$transID.'</p><br/><p style="margin:0;"><b>Voucher IDs:</b></p><p style="margin:0;">'.$voucherString.'</p><br/><p style="margin:0;"><b>How-To-Use This Date Package:</b></p><p style="margin:0;">1. Make your reservation now by calling '.$bname.' at '.$phone.'.</p><p style="margin:0;">2. Print & bring your ForTwoPlease Voucher, which is available on <a href="http://www.fortwoplease.com/vancouver/myaccount">your account page</a>.</p><br/><p style="margin:0;">(Reservations are required for all ForTwoPlease Date Packages)</p><br/><p style="margin:0;">Enjoy!</p><br/><p style="margin:0;">The ForTwoPlease Team</p>
-            <br/><p style="margin:0;">p.s. Have any questions or need some help? Email us at <b>support@fortwoplease.com</b> and we\'ll get back to you as soon as we can!</p><br/><p style="margin:0;"><a href="http://www.fortwoplease.com/vancouver/myaccount">Take me to my account page</a></p><p style="margin:0;"><a href="http://www.fortwoplease.com/">Discover more date ideas!</a></p>',$headers);
 
+            // Send email using Mandrill API.
+            $html = '<p style="margin:0;"><strong>Congratulations,</strong></p><p style="margin:0;">Your purchase of '.$numberVouchers.' of '.$pname.' from '.$bname.' was successful!</p><br/><p style="margin:0;"><b>Payment Summary</b></p><p style="margin:0;">Total: $'.$price.'</p><p style="margin:0;">Confirmation Number: '.$transID.'</p><br/><p style="margin:0;"><b>Voucher IDs:</b></p><p style="margin:0;">'.$voucherString.'</p><br/><p style="margin:0;"><b>How-To-Use This Date Package:</b></p><p style="margin:0;">1. Make your reservation now by calling '.$bname.' at '.$phone.'.</p><p style="margin:0;">2. Print & bring your ForTwoPlease Voucher, which is available on <a href="http://www.fortwoplease.com/vancouver/myaccount">your account page</a>.</p><br/><p style="margin:0;">(Reservations are required for all ForTwoPlease Date Packages)</p><br/><p style="margin:0;">Enjoy!</p><br/><p style="margin:0;">The ForTwoPlease Team</p><br/><p style="margin:0;">p.s. Have any questions or need some help? Email us at <b>support@fortwoplease.com</b> and we\'ll get back to you as soon as we can!</p><br/><p style="margin:0;"><a href="http://www.fortwoplease.com/vancouver/myaccount">Take me to my account page</a></p><p style="margin:0;"><a href="http://www.fortwoplease.com/">Discover more date ideas!</a></p>';
+			$to = array(array('email'=>$email, 'type'=>'to'));
+            $message = array('html'=> $html, 'subject' => 'Purchase Successful!', 'from_email'=>'info@fortwoplease.com', 'from_name'=> 'ForTwoPlease', 'to'=> $to);
+            $data = json_encode(array('key'=>'OybeEIWO9N2oDsURJI3qmg', 'message' => $message));
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($curl, CURLOPT_URL, 'https://mandrillapp.com/api/1.0/messages/send.json');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($curl);
             // Add transaction meta data to usermeta table.
             date_default_timezone_set('Canada/Pacific');
             $timestamp =  date("Y-m-d H:i:s");
@@ -473,10 +481,10 @@ function pp_action() {
                     add_user_meta(1,'sold',$summary);
             }
             
+            // Display confirmation to user.
             $message = "<div style='min-height:300px;background:#231f20;color:#FFF;padding-left:15px;'><div style='color:white;width:320px;height:40px;'><h1 style='float:left;margin-left:0px;'>SUCCESS!</h1><img style='float:right;margin-top:5px;margin-right:20px;' src='/dev/wp-content/themes/images/step3.png' /></div><div style='float:left;clear:both;'><p><b>Your card has been charged $".$price.".</b></p><br/><p>Make your reservation now by calling <b>".$bname."</b> at <b>".$phone."</b>.</p><br/><p>Just remember to take your ForTwoPlease Voucher, which is located on <a href='/dev/myaccount'>your account page</a>.</p><br/><p>We've also sent you an email for reference, with your confirmation code, <b>".$transID."</b>.</p><br/><p>Have a great date!</p><br/><a href='/dev/myaccount'>Your Account</a><br/><a href='/vancouver/date-idea-type/packages'>< Browse More Date Packages!</a></div></div>";
             $tax = $price - ($price_per_item * $numberp);
             $ga_data = array('transID' => $transID, 'merchantName' => $bname, 'total' => $price, 'tax' => $tax, 'price_per_item' => $price_per_item, 'category' => '', 'productName' => $pname, 'quantity' => $numberp);
-
             $array = array('result' => 0, 'email' => "anuj.nm@gmail.com", 'price' => $price, 'message' => $message, 'ga_data' => $ga_data);
             echo json_encode($array);
 		}
