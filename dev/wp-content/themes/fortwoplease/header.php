0013a42944aac953abe2b330af00ca1e4db1c6db
Anuj Mehta <anuj.nm@gmail.com>
2014-01-15 15:44:46 +0530

    Some CSS updates for viewing the site on mobile

--- dev/wp-content/themes/fortwoplease/header.php
+++ dev/wp-content/themes/fortwoplease/header.php
@@ -42,7 +42,7 @@
 	?>
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?php
 		/*
 		 * Print the <title> tag based on what is being viewed.
