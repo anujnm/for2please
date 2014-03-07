8bd2c6e5a0617542aa9230f9ce485259a45fd13a
Anuj Mehta <anuj.nm@gmail.com>
2014-03-07 16:14:14 +0530

    Welcome email will now be sent via Mandrill.

--- dev/wp-content/themes/fortwoplease/inc/config.inc.php
+++ dev/wp-content/themes/fortwoplease/inc/config.inc.php
@@ -1,17 +1,16 @@
 <?php
     //API Key - see http://admin.mailchimp.com/account/api
-    $apikey = 'a9b59d187d50e29bab9faf83cf90bdc0-us5';
+    //$apikey = 'a9b59d187d50e29bab9faf83cf90bdc0-us5';
+    $apikey = 'd839622c7af9d4fe685bd50065f95a91-us3';
     
     // A List Id to run examples against. use lists() to view all
     // Also, login to MC account, go to List, then List Tools, and look for the List ID entry
-    $listId = '823b1f2498';
-    
-    // A Campaign Id to run examples against. use campaigns() to view all
-    $campaignId = '117981';
+    //$listId = '823b1f2498';
+    $listId = '7628e10649';
 
     //some email addresses used in the examples:
-    $my_email = 'info@fortwoplease.com';
-    $boss_man_email = 'info@fortwoplease.com';
+    $my_email = 'anuj@fortwoplease.com';
+    $boss_man_email = 'anuj@fortwoplease.com';
 
     //just used in xml-rpc examples
     $apiUrl = 'http://api.mailchimp.com/1.3/';
